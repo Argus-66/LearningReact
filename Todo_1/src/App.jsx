edf3da41223400654dc97dfe30d0d83d350c0eeb
Argus-66 <jqghebri@gmail.com>
2024-08-27 19:45:39 +0530

    Commit

--- Todo_1/src/App.jsx
+++ Todo_1/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { TodoProvider } from './Contexts'
 import './App.css'
 import useTodos from './Functions/Todos'
+import { TodoForm }from './Components/index';
+import { TodoItems } from './Components/index';
 
 function App() {
     const { todos, addTodo, updateTodo, deleteTodo, toggleTodo } = useTodos();
@@ -13,9 +15,11 @@ function App() {
             <h1 className='text-2xl font-bold text-center mb-8 mt-2'>Manage Your Todos</h1>
             <div className='mb-4'>
                 {/* Todo from goes here */}
+                <TodoForm />
             </div>
             <div className='flex flex-wrap gap-y-3'>
                 {/* Loop and Add Todo Item here */}
+                {/* <TodoItem /> */}
             </div>
         </div>
       </div>
